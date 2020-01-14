f948a1058dffe50f924127e4b108de4eaa962c99
Tzion Beniaminov <tzion14@gmail.com>
2020-01-14 12:48:02 +0200

    finishet to draw the graphs with out the robbats and the fruits stil

--- src/gameClient/MyGameGUI.java
+++ src/gameClient/MyGameGUI.java
@@ -36,28 +36,77 @@ public class MyGameGUI {
 		DGraph gg = new DGraph();
 		gg.init(g);
 		drawNodes(gg);
+		drawEdges(gg);
 		String info = game.toString();
 		List<String> fruits=this.game.getFruits();
 		Iterator<String> f_iter = fruits.iterator();
 		while(f_iter.hasNext()) {
 			fruit f1=new fruit(f_iter.next());
 		}
-		
 
+		/**
+		 * this method drawing all the vertices of the graph 
+		 */
 	}
-	public void drawNodes(DGraph gg) {
+	private void drawNodes(DGraph gg) {
+		setRangeScale(gg);
 		Collection<node_data> vert=gg.getV();
 		Iterator<node_data> vert_it=vert.iterator();
 		StdDraw.setPenColor(Color.red);
-		StdDraw.setPenRadius(0.005);
-		StdDraw.setXscale(-100, 100);
-		StdDraw.setYscale(-50, 50);
+		StdDraw.setPenRadius(0.040);
 		while(vert_it.hasNext()) {
 			node_data tempV=vert_it.next();
 			Point3D pp=new Point3D(tempV.getLocation());
-			
 			StdDraw.point(pp.x(), pp.y());
 		}
+	}
+
+	/**
+	 * this method is drawing all the edges of the graph
+	 * @param gg
+	 */
+	private void drawEdges(DGraph gg) {
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.setPenRadius(0.005);
+		Collection<node_data> vert=gg.getV();
+		Iterator<node_data> vert_it=vert.iterator();
+		while(vert_it.hasNext()) {
+			node_data tempV=vert_it.next();
+			Collection<edge_data> egdesVert=gg.getE(tempV.getKey());
+			Iterator<edge_data> egdesVert_it=egdesVert.iterator();
+			while(egdesVert_it.hasNext()) {
+				edge_data ee=egdesVert_it.next();
+				Point3D srcV=new Point3D(gg.getNode(ee.getSrc()).getLocation());
+				Point3D destV=new Point3D(gg.getNode(ee.getDest()).getLocation());
+				StdDraw.line(srcV.x(), srcV.y(), destV.x(), destV.y());
+			}
+		}
+	}
+	
+	/**
+	 * this method gets the graph nodes and calculating the range of the X and 
+	 * Y axis
+	 * @param gg
+	 */
+	private void setRangeScale (DGraph gg) {
+		double minX=Double.MAX_VALUE,minY=Double.MAX_VALUE;
+		double maxX=Double.MIN_VALUE,maxY=Double.MIN_VALUE;
+		Collection<node_data> vert=gg.getV();
+		Iterator<node_data> vert_it=vert.iterator();
+		while(vert_it.hasNext()) {
+			node_data tt=vert_it.next();
+			Point3D pp=new Point3D(tt.getLocation());
+			if(pp.x()>maxX) 
+				maxX=pp.x();
+			if(pp.x()<minX) 
+				minX=pp.x();
+			if(pp.y()>maxY)
+				maxY=pp.y();
+			if(pp.y()<minY)
+				minY=pp.y();
+		}
+		StdDraw.setXscale(minX-0.00020, maxX+0.00020);
+		StdDraw.setYscale(minY-0.00020, maxY+0.00020);		
 	}
 
 
@@ -74,7 +123,7 @@ public class MyGameGUI {
 			level=Integer.parseInt(scenario_num);
 		}
 		this.game=Game_Server.getServer(level);
-		StdDraw.setCanvasSize(1000, 625);
+		StdDraw.setCanvasSize(1200, 640);
 		GraphInit();
 	}
 
